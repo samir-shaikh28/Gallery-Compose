2d96223702a7e5d8c89cd7c77a4493942a3cd7a0
samirshaikh <sksamir09@gmail.com>
2024-06-22 00:25:28 +0530

    Hilt dependencies added, Basic DI setup done.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -56,6 +59,11 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    // Hilt - DI
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+    
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/droidtechlab/composegallery/ComposeGalleryApp.kt
@@ -0,0 +1,8 @@
+package com.droidtechlab.composegallery
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ComposeGalleryApp: Application() {
+}
