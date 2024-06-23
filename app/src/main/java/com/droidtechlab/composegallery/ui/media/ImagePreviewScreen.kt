f6bbbfeda44f0f0fb95d812ab679d94152adfe99
samirshaikh <sksamir09@gmail.com>
2024-06-24 02:32:10 +0530

    ImagePreviewScreen added

--- app/src/main/java/com/droidtechlab/composegallery/ui/media/ImagePreviewScreen.kt
+++ app/src/main/java/com/droidtechlab/composegallery/ui/media/ImagePreviewScreen.kt
@@ -1,13 +1,35 @@
 package com.droidtechlab.composegallery.ui.media
 
 import android.net.Uri
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import coil3.compose.LocalPlatformContext
+import coil3.compose.rememberAsyncImagePainter
+import coil3.request.CachePolicy
+import coil3.request.ImageRequest
+import coil3.size.Scale
 
 @Composable
 fun ImagePreviewScreen(
     uri: Uri,
-    navController: NavController
 ) {
+    val painter = rememberAsyncImagePainter(
+        model = ImageRequest.Builder(LocalPlatformContext.current)
+            .data(uri)
+            .memoryCachePolicy(CachePolicy.ENABLED)
+            .diskCachePolicy(CachePolicy.ENABLED)
+            .placeholderMemoryCacheKey(uri.toString())
+            .scale(Scale.FILL)
+            .build(),
+        contentScale = ContentScale.FillBounds
+    )
+    Image(
+        modifier = Modifier.fillMaxSize(),
+        painter = painter,
+        contentDescription = "",
+    )
 
 }
